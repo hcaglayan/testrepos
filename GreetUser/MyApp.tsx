37561dd1ff835347c8b82986d4f7b53f34298823
Huseyin Caglayan <Huseyin.Caglayan@dpr.co.uk>
2018-07-16 14:41:09 +0000

    added
    git-tfs-id: [http://tfscore:8080/tfs/core]$/Trash/InternalSeminars/React-20180719;C102463

---
+++ GreetUser/MyApp.tsx
@@ -0,0 +1,54 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+interface MyComponentProps
+{
+    person: string;
+}
+
+interface MyComponentState
+{
+    greetingAcknowledged: boolean;
+}
+
+class MyComponent extends React.Component<MyComponentProps, MyComponentState>
+{
+	constructor(props: MyComponentProps)
+    {
+        super(props);
+		this.state = { greetingAcknowledged: false } as MyComponentState;
+	}
+
+	private personAcknowledged() : void
+	{
+		this.setState( { greetingAcknowledged: true } );
+	}
+	
+	render(): JSX.Element | null | false
+	{
+		return (
+            <div>
+				<h3>Hello { this.props.person }. Welcome to our App. Click the button to Acknowledge our greeting</h3>
+				{ !this.state.greetingAcknowledged ? <button onClick= { (e) => { this.personAcknowledged() } } >Acknowledge</button> :  <p>All acknowledged and we are happy </p> }
+            </div>
+        );
+	}
+
+}
+
+class MyApp
+{
+	public bootUp()
+    {
+		ReactDOM.render(
+            <MyComponent>
+            </MyComponent>,
+            document.querySelector("#myAppStartsHere")
+        );
+    }
+}
+
+let myApp = new MyApp();	
+myApp.bootUp();
+
---
+++ GreetUser/systemjs.config.js
@@ -0,0 +1,36 @@
+﻿System.config({
+    "transpiler": "ts",
+    "typescriptOptions": {
+        "target": "es5",
+        "module": "system",
+        "moduleResolution": "node",
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "jsx": "react"
+    },
+    "meta": {
+        "typescript": {
+            "exports": "ts"
+        },
+        "*.json": { "loader": "plugin-json" }
+    },
+    "paths": {
+    },
+    "map": {
+        "app": "/",
+        "ts": "https://unpkg.com/plugin-typescript@4.0.10/lib/plugin.js",
+        "typescript": "https://unpkg.com/typescript@2.2.2/lib/typescript.js",
+        "plugin-json": "https://cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.3.0/json.min.js",
+        "react-dom": "https://unpkg.com/react-dom@15.5.4/dist/react-dom.min.js",
+        "react": "https://unpkg.com/react@15.5.4/dist/react.min.js"
+
+    },
+    "packages": {
+        "app": {
+            "main": "MyApp",
+            "defaultExtension": "tsx"
+        }
+    }
+}); 
+
+System.import('app');
