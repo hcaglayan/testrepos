02e1edfd9b6ea8d603a5dbf9e013a8bc06319cf6
Huseyin Caglayan <Huseyin.Caglayan@dpr.co.uk>
2018-07-16 14:08:14 +0000

    very first React app
    git-tfs-id: [http://tfscore:8080/tfs/core]$/Trash/InternalSeminars/React-20180719;C102459

---
+++ HelloReactApp/MyApp.tsx
@@ -0,0 +1,21 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+class MyApp
+{
+	public bootUp()
+    {
+		ReactDOM.render(
+            <div>
+				<h2>Hello React</h2>
+				<p>This example demonstrates how the React rendering is transpiled to JavaScript</p>
+            </div>,
+            document.querySelector("#myAppStartsHere")
+        );
+    }
+}
+
+let myApp = new MyApp();	
+myApp.bootUp();
+
